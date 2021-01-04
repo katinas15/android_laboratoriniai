067442d3ee36ff4666143d566c197115f3260122
katinas15 <laimonas.janutenas@gmail.com>
2021-01-04 11:10:43 +0200

    ...

--- Kontroliniai/4variantasK2 - Copy (2)/app/src/main/java/com/example/a4variantask2/IvestisFragment.java
+++ Kontroliniai/4variantasK2 - Copy (2)/app/src/main/java/com/example/a4variantask2/IvestisFragment.java
@@ -1,14 +1,19 @@
 package com.example.a4variantask2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class IvestisFragment extends Fragment {
@@ -19,6 +24,7 @@ public class IvestisFragment extends Fragment {
     EditText b;
     EditText min;
     EditText max;
+    TextView text;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -29,6 +35,8 @@ public class IvestisFragment extends Fragment {
         b = v.findViewById(R.id.b);
         min = v.findViewById(R.id.min);
         max = v.findViewById(R.id.max);
+        text = v.findViewById(R.id.context2);
+        registerForContextMenu(text);
         button1.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -40,6 +48,31 @@ public class IvestisFragment extends Fragment {
 
         return v;
 
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getActivity().getMenuInflater().inflate(R.menu.click_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.apie:
+                Intent langas = new Intent(getActivity(), ApieActivity.class);
+
+                startActivity(langas);
+                return true;
+            case R.id.kaip:
+                Intent langas1 = new Intent(getActivity(), KaipActivity.class);
+
+                startActivity(langas1);
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
 
